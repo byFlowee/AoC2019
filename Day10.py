7a65dbc5c1d002b409a663db0256f09f06bfc194
Jordi Amoros Moreno <amoros@npdu1475.bcn.rd.hpicorp.net>
2019-12-10 18:33:56 +0100

    init

---
+++ Day10.py
@@ -0,0 +1,64 @@
+def parse_input():
+    return [[y for y in x] for x in open('test', 'r').read().split('\n')]
+
+data = parse_input()
+
+# y = mx + c 
+def trace_line(x1,y1,x2,y2):
+
+    # same points
+    if x1 == x2 and y1 == y2:
+        return ()
+
+    # slope is zero, collision is along y axis
+    if x1 == x2:
+        step = 1 if y1 < y2 else -1
+        for y in range(y1, y2+step, step):
+            #print(x1, y)
+            if data[y][x1] == '#':
+                return (x1,y)
+
+        return ()
+
+    # slope
+    m = (y2 - y1) / (x2 - x1)
+    # axis offset 
+    c = y1 - m*x1
+
+    step = 1 if x1 < x2 else -1
+    for x in range(x1+step, x2+step, step):
+        y = m*x + c
+        if y.is_integer():
+            #print (x, int(y))
+
+            if data[int(y)][x] == '#':
+                return (x, int(y))
+
+    return ()            
+
+def compute_observable_asteroids(x, y):
+    visible = set()
+    for i in range(len(data)):
+        visible.add(trace_line(x,y,i,0))
+        visible.add(trace_line(x,y,i,len(data[0])-1))
+    for j in range(len(data[0])):
+        visible.add(trace_line(x,y,0,j))
+        visible.add(trace_line(x,y,len(data)-1, j))
+    
+    return len(visible) - 1
+    
+
+def get_max_visible():
+    best = 0
+    best_xy = (0,0)
+    for current_x in range(len(data)):
+        for current_y in range(len(data[0])):
+            if data[current_y][current_x] == '#':
+                n = compute_observable_asteroids(current_x, current_y)
+                if n > best:
+                    best = n
+                    best_xy = (current_x, current_y)  
+
+    return best_xy, best
+
+print(get_max_visible())
